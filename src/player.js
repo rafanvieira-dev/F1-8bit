21cf9e62e3f2bc5207d0eac4b13e2c50b04e0220
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 12:50:24 -0300

    Add Player class with movement logic

--- src/player.js
+++ src/player.js
@@ -1,1 +1,17 @@
+export class Player{
+    constructor(){
+        this.x = 240;
+        this.y = 520;
+        this.speed = 4;
+    }
+
+    update(input){
+        if(input.left) this.x -= this.speed;
+        if(input.right) this.x += this.speed;
+
+        // limite da pista
+        if(this.x < 140) this.x = 140;
+        if(this.x > 340) this.x = 340;
+    }
+}
 
