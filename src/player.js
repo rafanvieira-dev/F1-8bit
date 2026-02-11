8a5e1db4d6eaabf188d998ffdec1fc29cf9d5c46
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 15:03:30 -0300

    Update player.js

--- src/player.js
+++ src/player.js
@@ -1,30 +1,27 @@
 export class Player{
     constructor(){
         this.lanes=[150,210,270,330];
-        this.currentLane=1;
-
-        this.x=this.lanes[this.currentLane];
+        this.lane=1;
+        this.x=this.lanes[this.lane];
         this.targetX=this.x;
-
         this.y=520;
-        this.switchCooldown=0;
+        this.cooldown=0;
     }
 
     update(input){
+        if(this.cooldown>0) this.cooldown--;
 
-        if(this.switchCooldown>0) this.switchCooldown--;
-
-        if(input.left && this.switchCooldown===0){
-            this.currentLane=Math.max(0,this.currentLane-1);
-            this.switchCooldown=8;
+        if(input.left && this.cooldown===0){
+            this.lane=Math.max(0,this.lane-1);
+            this.cooldown=8;
         }
 
-        if(input.right && this.switchCooldown===0){
-            this.currentLane=Math.min(3,this.currentLane+1);
-            this.switchCooldown=8;
+        if(input.right && this.cooldown===0){
+            this.lane=Math.min(3,this.lane+1);
+            this.cooldown=8;
         }
 
-        this.targetX=this.lanes[this.currentLane];
-        this.x += (this.targetX - this.x) * 0.25;
+        this.targetX=this.lanes[this.lane];
+        this.x+= (this.targetX-this.x)*0.25;
     }
 }
