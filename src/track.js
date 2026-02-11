0dcc3fdd5de55dda3b1ce5ebadc07359042f86ff
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 14:36:50 -0300

    Refactor Track class into drawTrack function

--- src/track.js
+++ src/track.js
@@ -1,11 +1,24 @@
-export class Track{
-    constructor(){
-        this.offset=0;
-        this.speed=3;
+export function drawTrack(ctx,track){
+
+    // asfalto
+    ctx.fillStyle="#3a3a3a";
+    ctx.fillRect(120,0,240,640);
+
+    // bordas vermelhas/brancas
+    for(let y=track.offset%40; y<640; y+=40){
+
+        ctx.fillStyle="#ff0000";
+        ctx.fillRect(120,y,10,20);
+        ctx.fillRect(350,y,10,20);
+
+        ctx.fillStyle="#ffffff";
+        ctx.fillRect(120,y+20,10,20);
+        ctx.fillRect(350,y+20,10,20);
     }
 
-    update(score){
-        this.speed = 3 + score/600; // acelera com o tempo
-        this.offset+=this.speed;
+    // linhas centrais (tracejadas)
+    ctx.fillStyle="#ffffff";
+    for(let y=track.offset%60; y<640; y+=60){
+        ctx.fillRect(238,y,4,30);
     }
 }
