08a0fc2ebbe752ef013bd0846a9b3f17a7aa4987
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 14:09:43 -0300

    Update enemies.js

--- src/enemies.js
+++ src/enemies.js
@@ -1,45 +1,38 @@
 export class Enemies{
     constructor(){
         this.list=[];
-        this.spawnTimer=0;
-        this.speed=3;
-        this.lastLane = 240;
-
+        this.timer=0;
+        this.patternDelay=90;
     }
 
-    update(trackSpeed){
-        this.spawnTimer++;
+    update(speed){
+        this.timer++;
 
-        // aumenta velocidade junto com a pista
-        this.speed = trackSpeed;
+        if(this.timer>this.patternDelay){
+            this.timer=0;
 
-        // cria inimigos
-        if(this.spawnTimer > 80 - Math.min(trackSpeed*5,50)){
-   this.spawnTimer=0;
+            const lanes=[150,210,270,330];
 
-            const lanes=[160,240,320];
-            let lane;
-do{
-    lane=lanes[Math.floor(Math.random()*lanes.length)];
-}while(Math.abs(lane - this.lastLane) < 60);
+            // escolhe a faixa segura
+            const safeLane=Math.floor(Math.random()*4);
 
-this.lastLane = lane;
+            for(let i=0;i<4;i++){
+                if(i===safeLane) continue;
 
-
-            this.list.push({
-                x:lane,
-                y:-40,
-                w:16,
-                h:32
-            });
+                this.list.push({
+                    lane:i,
+                    x:lanes[i],
+                    y:-60,
+                    w:18,
+                    h:40
+                });
+            }
         }
 
-        // move inimigos
         for(let e of this.list){
-            e.y+=this.speed;
+            e.y+=speed;
         }
 
-        // remove fora da tela
         this.list=this.list.filter(e=>e.y<700);
     }
 }
