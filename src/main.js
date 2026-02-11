484b5b895d180c24a64ea57528654fca633484ce
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 15:13:13 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import {input} from "./input.js";
 import {Player} from "./player.js";
 import {Track} from "./track.js";
-import {drawTrack,drawCar,drawHUD} from "./renderer.js";
+import {drawTrack,drawCar,drawEnemies,drawHUD} from "./renderer.js";
 
 const canvas=document.getElementById("game");
 const ctx=canvas.getContext("2d");
@@ -12,11 +12,24 @@ const track=new Track();
 let start=Date.now();
 let gameOver=false;
 
+function checkCollision(){
+
+    for(const e of track.enemies){
+
+        if(Math.abs(e.x-player.x)<20 &&
+           Math.abs(e.y-player.y)<25){
+            return true;
+        }
+    }
+    return false;
+}
+
 function endGame(){
     gameOver=true;
+
     const time=((Date.now()-start)/1000).toFixed(1);
 
-    let name=prompt("NOVO RECORDE! 3 letras:");
+    let name=prompt("BATEU! 3 letras:");
     if(!name) name="???";
     name=name.substring(0,3).toUpperCase();
 
@@ -33,13 +46,14 @@ function update(){
     player.update(input);
     track.update();
 
-    if(track.speed>18) endGame(); // condição provisória
+    if(checkCollision()) endGame();
 }
 
 function render(){
     ctx.clearRect(0,0,480,640);
 
     drawTrack(ctx,track);
+    drawEnemies(ctx,track);
     drawCar(ctx,player);
 
     const time=((Date.now()-start)/1000).toFixed(1);
