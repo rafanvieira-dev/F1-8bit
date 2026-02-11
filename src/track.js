a1c2e5d4bea40361b2e6bb24944f39dad8dc0866
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 12:57:42 -0300

    Refactor Track class update method and properties

--- src/track.js
+++ src/track.js
@@ -1,11 +1,11 @@
 export class Track{
     constructor(){
-        this.offset = 0;
-        this.speed = 3;
+        this.offset=0;
+        this.speed=3;
     }
 
-    update(){
-        this.offset += this.speed;
+    update(score){
+        this.speed = 3 + score/600; // acelera com o tempo
+        this.offset+=this.speed;
     }
 }
-
