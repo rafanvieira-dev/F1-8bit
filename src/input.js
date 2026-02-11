6b825ddcfc571928675bcc0452e505de582dc69d
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 12:49:59 -0300

    Add input handling for left and right arrow keys

--- src/input.js
+++ src/input.js
@@ -1,1 +1,15 @@
+export const input = {
+    left:false,
+    right:false
+};
+
+window.addEventListener("keydown", e=>{
+    if(e.key==="ArrowLeft") input.left=true;
+    if(e.key==="ArrowRight") input.right=true;
+});
+
+window.addEventListener("keyup", e=>{
+    if(e.key==="ArrowLeft") input.left=false;
+    if(e.key==="ArrowRight") input.right=false;
+});
 
