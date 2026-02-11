955c786ad8645025e0784a5e02548f1f8e4dcce5
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 12:51:37 -0300

    Initialize game with player, track, and rendering

--- src/game.js
+++ src/game.js
@@ -1,1 +1,35 @@
+import {input} from "./input.js";
+import {Player} from "./player.js";
+import {Track} from "./track.js";
+import {drawTrack, drawCar, drawCockpit} from "./renderer.js";
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const player = new Player();
+const track = new Track();
+
+let score = 0;
+
+function update(){
+    player.update(input);
+    track.update();
+    score++;
+}
+
+function render(){
+    ctx.clearRect(0,0,480,640);
+
+    drawTrack(ctx,track);
+    drawCar(ctx,player);
+    drawCockpit(ctx,score);
+}
+
+function loop(){
+    update();
+    render();
+    requestAnimationFrame(loop);
+}
+
+loop();
 
