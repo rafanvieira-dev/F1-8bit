9d7e3723ea31d9b1f2906e35c174e9305b1fa493
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-11 12:57:23 -0300

    Refactor drawing functions and add drawEnemies

--- src/renderer.js
+++ src/renderer.js
@@ -1,27 +1,31 @@
 export function drawTrack(ctx, track){
-
-    // asfalto
     ctx.fillStyle="#2b2b2b";
     ctx.fillRect(120,0,240,640);
 
-    // linhas centrais
     ctx.fillStyle="#ffffff";
     for(let i=0;i<20;i++){
-        ctx.fillRect(238, (i*40 - track.offset%40), 4,20);
+        ctx.fillRect(238,(i*40-track.offset%40),4,20);
     }
 }
 
 export function drawCar(ctx, player){
     ctx.fillStyle="red";
-    ctx.fillRect(player.x-8, player.y-16, 16,32);
+    ctx.fillRect(player.x-8,player.y-16,16,32);
 }
 
-export function drawCockpit(ctx, score){
+export function drawEnemies(ctx,enemies){
+    ctx.fillStyle="yellow";
+    for(let e of enemies.list){
+        ctx.fillRect(e.x-8,e.y-16,e.w,e.h);
+    }
+}
+
+export function drawCockpit(ctx,score,speed){
     ctx.fillStyle="black";
     ctx.fillRect(0,560,480,80);
 
     ctx.fillStyle="#00ff00";
     ctx.font="16px monospace";
-    ctx.fillText("TIME: "+score, 350,600);
+    ctx.fillText("TIME: "+score,300,590);
+    ctx.fillText("KMH: "+Math.floor(speed*40),300,610);
 }
-
