9db2c76528f8527c6631ae452dccb90e3effa209
rafanvieira-dev <rafan.vieira@gmail.com>
2026-03-16 17:54:51 -0300

    Update player.js

--- src/player.js
+++ src/player.js
@@ -2,14 +2,12 @@ export class Player {
     constructor(track) {
         this.track = track;
         this.x = track.roadLeft + (track.roadWidth / 2);
-        
-        // Limites de câmera para dar a sensação do carro indo e voltando
         this.yBottom = track.canvas.height - 120; 
         this.yTop = track.canvas.height / 2.2;    
         this.y = this.yBottom; 
         
         this.speed = 0;
-        this.maxSpeed = 250; // Começa em 250km/h
+        this.maxSpeed = 250; 
         this.accel = 1.6;
         this.braking = 3.5;
         this.friction = 0.5;
@@ -18,7 +16,7 @@ export class Player {
     }
 
     update(input, level) {
-        // Aumenta a velocidade máxima: +10km/h por nível, até o limite de 350km/h
+        // Velocidade aumenta 10km/h por nível até 350km/h
         this.maxSpeed = Math.min(350, 250 + (level - 1) * 10);
 
         if (input.up) {
@@ -34,28 +32,15 @@ export class Player {
 
         let speedFactor = this.speed / this.maxSpeed; 
         
-        if (input.left && this.speed > 0) {
-            this.x -= this.turnSpeed * (speedFactor + 0.4); 
-        }
-        if (input.right && this.speed > 0) {
-            this.x += this.turnSpeed * (speedFactor + 0.4);
-        }
+        if (input.left && this.speed > 0) this.x -= this.turnSpeed * (speedFactor + 0.4); 
+        if (input.right && this.speed > 0) this.x += this.turnSpeed * (speedFactor + 0.4);
 
         const leftEdge = this.track.roadLeft + 30;
         const rightEdge = this.track.roadLeft + this.track.roadWidth - 30;
 
-        if (this.x < leftEdge) {
-            this.x = leftEdge;
-            this.speed -= this.friction * 2; 
-        }
-        if (this.x > rightEdge) {
-            this.x = rightEdge;
-            this.speed -= this.friction * 2; 
-        }
-        
-        if (this.speed < 0) this.speed = 0;
+        if (this.x < leftEdge) { this.x = leftEdge; this.speed -= 1; }
+        if (this.x > rightEdge) { this.x = rightEdge; this.speed -= 1; }
 
-        // Efeito de câmera acelerando o carro pro meio da tela
         const targetY = this.yBottom - (speedFactor * (this.yBottom - this.yTop));
         this.y += (targetY - this.y) * 0.05;
     }
